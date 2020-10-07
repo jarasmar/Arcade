e5d299c2b5a9485696aa0261a5e0766a77b46f70
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-10-07 11:23:58 +0100

    Added authentication token code for current users

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -136,4 +136,13 @@ router.post(
     }
 );
 
+router.get("/me", auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.json(user);
+    } catch (e) {
+        res.send({ message: "Error in Fetching user" });
+    }
+});
+
 module.exports = router;
