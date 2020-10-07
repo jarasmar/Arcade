e48b621daf497c29e31873049bdad09a81c9aa55
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-10-07 10:58:43 +0100

    Added login page back-end code

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -73,4 +73,67 @@ router.post(
     }
 );
 
+router.post(
+    "/login",
+    [
+        check("username", "Please enter a valid username")
+        .not()
+        .isEmpty(),
+        check("password", "Please enter a valid password").isLength({
+            min: 6
+        })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const { username, password } = req.body;
+        try {
+            let user = await User.findOne({
+                username
+            });
+            if (!user)
+            return res.status(400).json({
+                message: "User Not Exist"
+            });
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch)
+            return res.status(400).json({
+                message: "Incorrect Password!"
+            });
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                "randomString",
+                {
+                    expiresIn: 3600
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                        token
+                    });
+                }
+            );
+        } catch (e) {
+            console.error(e);
+            res.status(500).json({
+                message: "Server Error"
+            });
+        }
+    }
+);
+
 module.exports = router;
