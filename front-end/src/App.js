3f0689639d6dd835b04f61c85d9b1e83da5de740
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-10-07 12:01:40 +0100

    Added template for sign in page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,14 +5,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home } from './components/homeComponent.js';
 import { Tetris } from './components/tetrisComponent.js';
+import { SignUp } from './components/signUpComponent.js';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
 
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/tetris" component={Tetris} />
+        <Route path="/" exact={true} component={ Home } />
+        <Route path="/tetris" component={ Tetris } />
+        <Route path="/signup" component={ SignUp } />
 
       </div>
 
